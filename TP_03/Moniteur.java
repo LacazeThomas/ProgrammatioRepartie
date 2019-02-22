0772631f85ce40cffd51d47f56f86f0ffc6ca62c
Thomas LACAZE <contact@thomaslacaze.fr>
2019-02-22 11:49:53 +0100

    Final tp03

--- TP_03/Moniteur.java
+++ TP_03/Moniteur.java
@@ -1,44 +1,45 @@
 import java.io.Console;
 public class Moniteur {
-
-	String buffer;
-	Boolean available;
+	
+	String[] lettre = {"a","b","c","d","*"};
+	int queue = 0;
+	int tete = 0;
+	String[] buffer = new String[lettre.length];
+	int available = 0;
 
 	public Moniteur(){
-		available = false;
-		buffer = "Init";
 	}
 
 	public synchronized void read(){
 		//Tant que occupé
-		while(available == false){
-			try {
-				wait(100);
-			} catch(InterruptedException e){
-			};
+		if(!lettre[queue].equals("*")){
+			while(available <= 0){
+				try {
+					wait(500);
+				} catch(InterruptedException e){
+				};
+			}
+			System.out.println(lettre[queue]);
+			queue++;
+			available--;
+			notifyAll();
 		}
-		System.out.println(buffer);
-		available = false;
-		notifyAll();
 	}
 
 	public synchronized void write(){
 		//Tant que pas occupé
-		while(available == true){
-			try {
-				wait(100);
-			} catch(InterruptedException e){
-			};
+		if(!lettre[tete].equals("*")){
+			while(available > lettre.length){
+				try {
+					wait(100);
+				} catch(InterruptedException e){
+				};
+			}
+			System.out.print("écriture\n");
+			buffer[tete] = lettre[tete];
+			available++;
+			tete++;
+			notifyAll();
 		}
-
-		Console console = System.console();
-		String input = console.readLine("Enter input:");
-
-		if(input.equals("q")){
-			System.exit(1);
-		}
-		buffer = input;
-		available = true;
-		notifyAll();
 	}
 }
